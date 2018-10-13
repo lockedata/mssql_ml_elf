5fbf0e6d48ee610c91d2b73986c1897489ea70ed
Steph Locke <steph@itsalocke.com>
2018-10-13 12:53:13 +0100

    Added predictions

---
+++ MSSQL_ML_ELF/Stored Procedures/ml.insert_prediction.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [ml].[insert_prediction]
+	@numeric_prediction float = NULL,
+	@string_prediction NVARCHAR(50) = NULL,
+	@features VARCHAR(MAX) = NULL,
+	@extra VARCHAR(MAX) = NULL
+AS
+	INSERT INTO [ml].[prediction] (
+	  numeric_prediction
+	, string_prediction
+	, features
+	, extra)
+	VALUES (
+	  @numeric_prediction
+	, @string_prediction
+	, @features
+	, @extra)
+
+RETURN SCOPE_IDENTITY()
---
+++ MSSQL_ML_ELF/Tables/ml.prediction.sql
@@ -0,0 +1,19 @@
+﻿/*
+The database must have a MEMORY_OPTIMIZED_DATA filegroup
+before the memory optimized object can be created.
+
+The bucket count should be set to about two times the 
+maximum expected number of distinct values in the 
+index key, rounded up to the nearest power of two.
+*/
+
+CREATE TABLE [ml].[prediction]
+(
+	[id] BIGINT NOT NULL PRIMARY KEY NONCLUSTERED HASH WITH (BUCKET_COUNT = 131072), 
+    [numeric_prediction] FLOAT NULL, 
+    [string_prediction] NVARCHAR(50) NULL, 
+    [features] VARCHAR(MAX) NULL, 
+    [extra] VARCHAR(MAX) NULL,
+    CONSTRAINT [CK_prediction_features] CHECK (ISJSON([features])=1), 
+    CONSTRAINT [CK_prediction_extra] CHECK (ISJSON([extra])=1), 
+) WITH (MEMORY_OPTIMIZED = ON)
