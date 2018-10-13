80263ecbd508ff80eb99b9bd831dff2b6a481b75
Steph Locke <steph@itsalocke.com>
2018-10-13 12:02:49 +0100

    dedicated schema and more detailed model store

---
+++ MSSQL_ML_ELF/Stored Procedures/ml.upsert_model.sql
@@ -0,0 +1,54 @@
+﻿CREATE PROCEDURE [ml].[upsert_model]
+  @name  NVARCHAR(50) 
+, @model VARBINARY(max) 
+, @training_n INT
+, @test_n INT
+, @key_metric VARCHAR(50)
+, @key_metric_val DECIMAL(10,10)
+, @metrics VARCHAR(MAX)
+, @info VARCHAR(MAX)
+
+AS
+WITH MySource as (
+    SELECT
+	  @name           AS [name] 
+	, @model          AS [model]
+	, @training_n     AS [training_observations]
+	, @test_n         AS [test_observations]
+	, @key_metric     AS [key_metric]
+	, @key_metric_val AS [key_metric_value]
+	, @metrics        as [metrics]
+	, @info           AS [info]
+)
+MERGE [ml].[model] AS MyTarget
+USING MySource
+     ON MySource.[name] = MyTarget.[name]
+WHEN MATCHED THEN UPDATE SET 
+      [model]                 = MySource.[model]
+	, [training_observations] = MySource.[training_observations]
+	, [test_observations]	  = MySource.[test_observations]
+	, [key_metric]			  = MySource.[key_metric]
+	, [key_metric_value]	  = MySource.[key_metric_value]
+	, [metrics]				  = MySource.[metrics]
+	, [info]				  = MySource.[info]
+WHEN NOT MATCHED THEN INSERT
+    (
+	  [name] 
+	, [model]
+	, [training_observations]
+	, [test_observations]
+	, [key_metric]
+	, [key_metric_value]
+	, [metrics]
+	, [info]
+    )
+    VALUES (
+	  MySource.[name] 
+	, MySource.[model]
+	, MySource.[training_observations]
+	, MySource.[test_observations]
+	, MySource.[key_metric]
+	, MySource.[key_metric_value]
+	, MySource.[metrics]
+	, MySource.[info]
+    );
---
+++ MSSQL_ML_ELF/Tables/ml.model.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [ml].[model]
+(
+	[id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [name] NVARCHAR(50) NOT NULL,
+    [model] varbinary(max) NOT NULL,
+	[training_observations] INT,
+	[test_observations] INT,
+	[key_metric] VARCHAR(50),
+	[key_metric_value] DECIMAL(10,10),
+	[metrics] VARCHAR(MAX),
+	[info] VARCHAR(MAX),
+	[SysStart] DATETIME2 (7) GENERATED ALWAYS AS ROW START NOT NULL,
+	[SysEnd] DATETIME2 (7) GENERATED ALWAYS AS ROW END NOT NULL, 
+    CONSTRAINT [CK_model_name] UNIQUE ([name]), 
+    CONSTRAINT [CK_model_metrics] CHECK (ISJSON([metrics])=1), 
+    CONSTRAINT [CK_model_info] CHECK (ISJSON([info])=1), 
+	PERIOD FOR SYSTEM_TIME ([SysStart], [SysEnd])
+)
+WITH (SYSTEM_VERSIONING = ON(HISTORY_TABLE=[ml].[model_HISTORY], DATA_CONSISTENCY_CHECK=ON))
